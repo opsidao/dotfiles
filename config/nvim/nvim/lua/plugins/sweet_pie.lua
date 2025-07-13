85deb383724ae23074650eee3f33cb81e28ed9a0
Juan González <juan.gonzalezaguilera@prima.it>
2025-07-13 16:58:50 +0200

    chore: Switch to lazyvim

---
+++ config/nvim/nvim/lua/plugins/sweet_pie.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "sweetpie.nvim",
+    url = "git@github.com:primait/sweetpie.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "colinkennedy/mega.cmdparse",
+      "colinkennedy/mega.logging",
+    },
+  },
+}
